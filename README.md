981bd370afb14aa9232c111349843671883d96a4
B Dowling <Blair0789@gmail.com>
2024-03-09 20:42:52 -0500

    project mostly done, need to add deployyed link to read me and an issue with local storage

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # My-Weather-Forecaster
+
+## Overview
+The My Weather Forecaster is for anyone especially travlers who want to stay up to date with the current and forecasted weather. This app allows the user to not only view weather both current and 5-day forecast, but also provides easy recall to previsouly searched cities, allowing the user to plan accordingly.
+
+## Features
+*Seacrh by city: This allow the user to search for thier desired city to view is current and forecasted weather.
+
+*Search History: This feature allows the user to easily go back to previsouly seached cities by selectin the city in the search history. The weather for the selected saved city will load onto the page.
+
+*Current Weather: A display of the city name, date, weather icon, humidity, temperature, and wind speed.
+
+*5-Day Forecast: This app allow the user to view the forecated weather for the city they have chosen. Displaying date, weather icon, humidity, temperature, and wind speed.
+
+## Technologies Used
+*OpenWeatherMap API (https://api.openweathermap.org)
+*HTML
+*CSS
+*JavaScript
+
+
+## Credits
+OpenWeatherMap API was use to provide the weather data for this app. 
+
+## How to Use
+*Enter the name of the city you wish to view weather for in the search bar.
+*The current weather and 5-day forecast will appear for your viewing pleasure.
+*To view a previsouly searched city's weather, just click on the city name in the history section.
+
+Created by: Blair Dowling
+
+<img src="./screenshot.png" width="650" height="350">
---
+++ script.js
@@ -0,0 +1,118 @@
+// OpenWeatherMap API key
+let apiKey = '5b22034f0ee272377858921625bd5d2b';
+
+//weather section variables defined
+let currentWeather = document.getElementById('currWeather');
+let forecastWeather = document.getElementById('forecaster');
+
+//saved city history variables defined
+let citiesSaved = JSON.parse(localStorage.getItem('weatherSearchHistory')) || [];
+let savedCity = document.createElement('li');
+let cityHistory = document.getElementById('searchHistory');
+let city = document.getElementById('citySearch').value;
+
+// submit button event listner for searcing cities
+document.getElementById('submitBtn').addEventListener('click', function() {
+    city;
+    //this will get the weather from the api 
+    getWeather(city); 
+    // this will add the searched city to the  users history section
+    addHistory(city); 
+});
+
+// this function will add the searched city to the history section
+function addHistory(city) {
+   
+    savedCity.textContent = city;
+    //this is an event listener for when the user clicks on the cities in history
+    savedCity.addEventListener('click', function() {
+         // this will load the previously saved city's weather
+        getWeather(city);
+    });
+    cityHistory.appendChild(savedCity);
+}
+
+// this function will use local storage to obtain the saved cities
+function recallSavedCities() { 
+    //this add the searched cities to the history 
+    citiesSaved.forEach(addHistory); 
+}
+
+// this will add the saved city to local storage
+function saveCitySearch(city) { 
+    if (!citiesSaved.includes(city)) {
+        citiesSaved.push(city);
+        localStorage.setItem('weatherSearchHistory', JSON.stringify(citiesSaved));
+    }
+}
+
+// this will display the saved cities when the window loads
+window.onload = recallSavedCities;
+
+// the function will get the city's current and forcasted weather for display
+function getWeather(city) {
+    
+    let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=imperial';
+    
+    // this fetch is for the current weather data
+    fetch(apiUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        //current weather display
+        displayWeather(data); 
+        //these are coordinates to obtain the forecasted weather
+        let latitude = data.coord.lat;
+        let longitude = data.coord.lon;
+        // this fetch is using the coordinated to obtain the forecated weather
+        return fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey + '&units=imperial');
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(forecastData) {
+        //forecasted weather display
+        displayForecast(forecastData); 
+    })
+    .catch(function(error) {
+        //will send an error for if one occurs when getting data
+        console.error('Error obtaining data: ', error);
+    });
+}
+
+// this function will display the current weather 
+function displayWeather(data) {
+    currentWeather ; 
+    // Formating the data for U.S. weather (in mph, °F, and date) 
+    let date = new Date(data.dt * 1000).toLocaleDateString();
+    //this is for the weather icon
+    let weatherIcon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+    //this will display all the data for the current weather
+    currentWeather.innerHTML = '<h1>'+data.name + '</h1>' + '<img src="' + weatherIcon + '">' + '<h1>' + date + ' </h1>' +  '<p>Temperature: ' + data.main.temp + ' °F</p>' +'<p>Wind Speed: ' + data.wind.speed + ' MPH</p>' +                                                                                 '<p>Humidity: ' + data.main.humidity + '%</p>';                               
+}
+
+// this function will display the forecasted weather 
+function displayForecast(datafcast) { 
+    forecastWeather.innerHTML = '';
+    // displays weather for each day using a loop
+    for (let i = 1; i < datafcast.list.length; i += 8) { 
+        let dataEachDay = datafcast.list[i];
+        //this will get the date for each forecasted day
+        let date = new Date(dataEachDay.dt_txt).toLocaleDateString();
+        //this will get the weather icon for each forecasted day
+        let weatherIcon = 'http://openweathermap.org/img/w/' + dataEachDay.weather[0].icon + '.png';
+        //this will create a div for each forecasted day
+        let day = document.createElement('div');
+        day.className = 'forecast-day';
+        //this will display the weather icon, date, temp, humidity, and wind speed for each of the forecasted days
+        day.innerHTML = '<img src="' + weatherIcon + '">' + '<h1>' + date + '</h1>' +'<p>Temp: ' + dataEachDay.main.temp + ' °F</p>' + '<p>Humidity: ' + dataEachDay.main.humidity + '%</p>' +'<p>Wind: ' + dataEachDay.wind.speed + ' MPH</p>';                      
+      forecastWeather.appendChild(day); 
+    }
+}
+
+
+
+
+
+
